b31149ac9527004ce5e9e8e7f9777b67b32eae0c
Ryan Duff <ryan@fusionized.com>
2015-10-15 11:21:01 -0400

    Wrap things in a singleton class for better hooking into WordPress

--- backupmigrate.php
+++ backupmigrate.php
@@ -10,85 +10,129 @@
 	Domain Path: /lang
  */
 
-use BackupMigrate\Core\Config\Config;
-
 require __DIR__.'/vendor/autoload.php';
 
-function backup_migrate_get_service_object( $config_array = [] ) {
+use BackupMigrate\Core\Config\Config;
 
-	static $bam = NULL;
+add_action( 'plugins_loaded', array ( BAM_WordPress::get_instance(), 'plugin_setup' ) );
 
-	// If the static cached object has not been loaded.
-	if ($bam === NULL) {
+class BAM_WordPress {
+	/**
+	 * Plugin instance.
+	 *
+	 * @see get_instance()
+	 * @type object
+	 */
+	protected static $instance = NULL;
 
-		// Create the environment services.
+	/**
+	 * Access this plugin’s working instance
+	 *
+	 * @wp-hook plugins_loaded
+	 * @since   0.1.0
+	 * @return  object of this class
+	 */
+	public static function get_instance() {
 
-		// Create the service locator
-		$services = new \BackupMigrate\Core\Service\ServiceLocator();
+		NULL === self::$instance and self::$instance = new self;
 
-		// Allow other wordpress plugins to add services.
-		$services = apply_filters( 'bam_service_locator', $services );
-
-		// Create the plugin manager
-		$plugins = new \BackupMigrate\Core\Plugin\PluginManager( $services );
-
-		// Allow other wordpress plugins to add plugins.
-		$plugins = apply_filters( 'bam_plugin_manager', $plugins, $config_array );
-
-		// Create the service object.
-		$bam = new \BackupMigrate\Core\Main\BackupMigrate( $plugins );
-
-		// Allow other modules to alter the BackupMigrate object
-		$bam = apply_filters( 'bam_service_object', $bam, $plugins );
-
-	}
-
-	// Set the configuration overrides if any were passed in.
-	if ( $config_array ) {
-
-		$bam->setConfig( new Config( $config_array ) );
+		return self::$instance;
 
 	}
 
-  return $bam;
+
+	/**
+	 * Used for regular plugin work.
+	 *
+	 * @wp-hook plugins_loaded
+	 * @since   0.1.0
+	 * @return  void
+	 */
+	public function plugin_setup() {
+
+		add_filter( 'bam_service_locator', array( $this, 'bam_override_service_locator' ), 10, 1 );
+		add_filter( 'bam_plugin_manager', array( $this, 'bam_override_plugin_manager' ), 10, 2 );
+
+		$bam = $this->backup_migrate_get_service_object();
+
+		$bam->backup( 'db', 'download' );
+
+	}
+
+
+	function backup_migrate_get_service_object( $config_array = [] ) {
+
+		static $bam = NULL;
+
+		// If the static cached object has not been loaded.
+		if ($bam === NULL) {
+
+			// Create the environment services.
+
+			// Create the service locator
+			$services = new \BackupMigrate\Core\Service\ServiceLocator();
+
+			// Allow other wordpress plugins to add services.
+			$services = apply_filters( 'bam_service_locator', $services );
+
+			// Create the plugin manager
+			$plugins = new \BackupMigrate\Core\Plugin\PluginManager( $services );
+
+			// Allow other wordpress plugins to add plugins.
+			$plugins = apply_filters( 'bam_plugin_manager', $plugins, $config_array );
+
+			// Create the service object.
+			$bam = new \BackupMigrate\Core\Main\BackupMigrate( $plugins );
+
+			// Allow other modules to alter the BackupMigrate object
+			$bam = apply_filters( 'bam_service_object', $bam, $plugins );
+
+		}
+
+		// Set the configuration overrides if any were passed in.
+		if ( $config_array ) {
+
+			$bam->setConfig( new Config( $config_array ) );
+
+		}
+
+	  return $bam;
+
+	}
+
+
+	function bam_override_service_locator( $services ) {
+
+		$services->add('TempFileAdapter',
+			new \BackupMigrate\Core\File\TempFileAdapter('/tmp/', 'bam')
+		);
+
+
+		$services->add('TempFileManager',
+			new \BackupMigrate\Core\File\TempFileManager($services->get('TempFileAdapter'))
+		);
+
+		return $services;
+
+	}
+
+
+	function bam_override_plugin_manager( $plugins, $config_array ) {
+
+		$connection = array(
+			'host'     => DB_HOST,
+			'username' => DB_USER,
+			'password' => DB_PASSWORD,
+			'database' => DB_NAME,
+		);
+
+		$db = new \BackupMigrate\Core\Source\MySQLiSource( new Config( $connection ) );
+		$plugins->add('db', $db);
+
+		$plugins->add('download', new \BackupMigrate\WordPress\Destination\WordPressBrowserDownloadDestination());
+
+		return $plugins;
+
+	}
 
 }
-
-add_filter( 'bam_service_locator', 'bam_override_service_locator', 10, 1 );
-function bam_override_service_locator( $services ) {
-
-	$services->add('TempFileAdapter',
-		new \BackupMigrate\Core\File\TempFileAdapter('/tmp/', 'bam')
-	);
-
-
-	$services->add('TempFileManager',
-		new \BackupMigrate\Core\File\TempFileManager($services->get('TempFileAdapter'))
-	);
-
-	return $services;
-
-}
-
-add_filter( 'bam_plugin_manager', 'bam_override_plugin_manager', 10, 2 );
-function bam_override_plugin_manager( $plugins, $config_array ) {
-
-	$connection = array(
-		'host'     => DB_HOST,
-		'username' => DB_USER,
-		'password' => DB_PASSWORD,
-		'database' => DB_NAME,
-	);
-
-	$db = new \BackupMigrate\Core\Source\MySQLiSource( new Config( $connection ) );
-	$plugins->add('db', $db);
-
-	$plugins->add('download', new \BackupMigrate\WordPress\Destination\WordPressBrowserDownloadDestination());
-
-	return $plugins;
-
-}
-
-$bam = backup_migrate_get_service_object();
-
-$bam->backup( 'db', 'download' );
