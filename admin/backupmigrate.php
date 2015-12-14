be613c9cc6c518adfc2ddf539c436521eac41646
Ryan Duff <ryan@fusionized.com>
2015-12-14 11:32:47 -0500

    Add form and submit button to backup page

--- admin/backupmigrate.php
+++ admin/backupmigrate.php
@@ -16,7 +16,13 @@ function bam_add_admin_menu() {
 function bam_admin_page() {
 
 	echo '<div class="wrap">';
-		echo '<h1>' . __( 'Backup and Migrate', 'bam' ) . '</h1>';
+		echo '<h1>' . __( 'Backup and Migrate', 'backupmigrate' ) . '</h1>';
+
+		echo '<form method="post">';
+			echo echo wp_nonce_field( 'bam_simple_backup', 'bam_simple_backup' );
+			submit_button( __( 'Backup Database', 'backupmigrate' ) );
+		echo '</form>';
+
 	echo '</div class="wrap">';
 
 }
